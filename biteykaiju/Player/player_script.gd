134754d37831b7052f3c8bcd5466278fb89aaa10
embersofintention <embersofintention@pm.me>
2025-07-11 16:59:52 -0400

    scripts written oh god i hop they wrok

---
+++ biteykaiju/Player/player_script.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+# variables 
+var player_scale = 1
+
+# constants
+const SPEED = 600
+
+# variables to call our part collection scenes
+@onready var body = %parts_body
+@onready var head = %parts_head
+
+# -------------------------------------------
+
+func _physics_process(delta: float) -> void:
+	
+	# get direction based on player input
+	var direction = Input.get_vector("move_down","move_left","move_right","move_up")
+	# set player velocity
+	velocity = direction * SPEED
+	
+	# character faces direction 
+	if direction:
+		# if moving horizontally:
+		if velocity.x != 0 and velocity.y == 0: 
+			# face the appropriate direction
+			body.scale.x = direction.x * player_scale
+		
+		# and to avoid scale.y breaking:
+		if velocity.y: 
+			body.scale.y = player_scale
